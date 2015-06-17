ea67b4b585fc6b5ef9e7a7971f0cb38dfe95bbe4
anuragshendge <shendge.anurag@gmail.com>
2015-06-17 10:46:29 -0400

    Fix CLL splitting. Add Extra check

--- split_cll.cpp
+++ split_cll.cpp
@@ -15,18 +15,70 @@ int main(){
 
 		Node *head = NULL;
 		Node *head1,*head2;
-
+		add_node(&head,99);
 		add_node(&head,12);
 		add_node(&head,56);
 		add_node(&head,2);
 		add_node(&head,11);
-
+		print(head);
 		split(head,&head1,&head2);
 		print(head1);
 		print(head2);
 
 }
 
-void split(Node *head, Node *head1_ref, Node **head2_ref){
-	
+
+void add_node(Node **head_ref, int value){
+	Node *new_node = new Node;
+	new_node->value = value;
+	Node *temp = *head_ref;
+	new_node->next = *head_ref;
+
+	if(*head_ref!=NULL){
+		while(temp->next != *head_ref)
+			temp = temp->next;
+		temp->next = new_node;
+	}
+	else
+		new_node->next = new_node;
+	*head_ref = new_node;
+}
+
+void print(Node *head){
+
+	Node *temp;
+	temp = head;
+
+	if(head!=NULL){
+
+		do{
+			cout<<" "<<temp->value;
+			temp = temp->next;
+		}
+		while(temp != head);
+		cout<<"\n";
+	}
+
+}
+
+
+void split(Node *head, Node **head1_ref, Node **head2_ref){
+
+		Node *slow = head;
+		Node *fast = head;
+		
+		while(fast->next!= head && fast->next->next != head){
+			slow = slow->next;
+			fast = fast->next->next;
+		}
+
+		if(fast->next->next == head)
+			fast = fast->next;
+		
+		*head1_ref = head;	
+		*head2_ref = slow->next;
+		fast->next = slow->next;
+		slow->next = head;
+		
+		
 }
