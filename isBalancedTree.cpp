d301956d5ac2f1605f76078829e91d565c41ecf4
anuragshendge <shendge.anurag@gmail.com>
2015-08-11 15:55:59 -0400

    Update cosmetich changes

--- isBalancedTree.cpp
+++ isBalancedTree.cpp
@@ -2,12 +2,28 @@
 
 using namespace std;
 
+
+/*
+How to determine if a binary tree is height-balanced?
+A tree where no leaf is much farther away from the root than any other leaf. Different balancing schemes allow different definitions of “much farther” and different amounts of work to keep them balanced.
+
+Consider a height-balancing scheme where following conditions should be checked to determine if a binary tree is balanced.
+An empty tree is height-balanced. A non-empty binary tree T is balanced if:
+1) Left subtree of T is balanced
+2) Right subtree of T is balanced
+3) The difference between heights of left subtree and right subtree is not more than 1.
+
+The above height-balancing scheme is used in AVL trees. The diagram below shows two trees, one of them is height-balanced and other is not. The second tree is not height-balanced because height of left subtree is 2 more than height of right subtree.
+
+*/
+
 struct Node{
 	
 	int value;
 	Node *left;
 	Node *right;
 };
+
 
 int isBalanced(Node *root, int* ht_ref){
 
